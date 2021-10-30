72765c692482463b8ff242e224378de7f045b2a6
Evgenii Grigorev <eagrigorev87@gmail.com>
2021-10-30 12:20:37 +0300

    Initial release

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: "./public/js/comp.js",
+    output: {
+        path: "E:/Code/clothing-store-landing-page/public/js",
+        filename: "./components.js",
+    },
+};
